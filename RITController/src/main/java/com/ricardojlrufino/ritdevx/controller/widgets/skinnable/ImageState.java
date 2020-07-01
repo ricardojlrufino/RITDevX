073b81ab7f4416c64009e793d339801682c332fc
Ricardo JL Rufino <ricardo.jl.rufino@gmail.com>
2020-07-01 20:35:23 -0300

    refractoring image widget - now is only a image no actions.

--- RITController/src/main/java/com/ricardojlrufino/ritdevx/controller/widgets/skinnable/ImageState.java
+++ RITController/src/main/java/com/ricardojlrufino/ritdevx/controller/widgets/skinnable/ImageState.java
@@ -1,18 +1,16 @@
 /*******************************************************************************
- * This file is part of RITDevX Controller project.
- * Copyright (c) 2020 Ricardo JL Rufino.
+ * This file is part of RITDevX Controller project. Copyright (c) 2020 Ricardo JL Rufino.
  *
- * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
- * the Free Software Foundation, version 3 with Classpath Exception.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, version 3 with Classpath
+ * Exception.
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program. See the included LICENSE file for details.
+ * You should have received a copy of the GNU General Public License along with this program. See
+ * the included LICENSE file for details.
  *******************************************************************************/
 package com.ricardojlrufino.ritdevx.controller.widgets.skinnable;
 
@@ -22,7 +20,6 @@ import java.io.File;
 import javax.swing.JComponent;
 import com.ricardojlrufino.ritdevx.controller.utils.UIHelper;
 import com.ricardojlrufino.ritdevx.controller.widgets.OnOffInterface;
-import com.ricardojlrufino.ritdevx.controller.widgets.OperationMode;
 
 /**
  * This component uses two images to represent the ON and OFF states (optional). <br/>
@@ -33,7 +30,7 @@ import com.ricardojlrufino.ritdevx.controller.widgets.OperationMode;
  * @author Ricardo JL Rufino - (ricardo.jl.rufino@gmail.com)
  * @date 11 de jun de 2020
  */
-public class ImageState extends JComponent implements OnOffInterface {
+public class ImageState extends JComponent implements OnOffInterface{
 
   private static final long serialVersionUID = 1L;
 
@@ -42,22 +39,14 @@ public class ImageState extends JComponent implements OnOffInterface {
   private boolean autoScale;
   private File imageOn;
   private File imageOff;
-  private String customCmd;
-  private OperationMode operationMode = OperationMode.NONE;
   
+  public ImageState() {
+    setAutoScale(true);
+  }
+
   // State
   private BufferedImage imageBufferON;
   private BufferedImage imageBufferOFF;
-
-  public OperationMode getOperationMode() {
-    return operationMode;
-  }
-
-  public void setOperationMode(OperationMode mode) {
-    this.operationMode = mode;
-  }
-
-
 
 
   public boolean isAutoScale() {
@@ -66,6 +55,7 @@ public class ImageState extends JComponent implements OnOffInterface {
 
   public void setAutoScale(boolean autoScale) {
     this.autoScale = autoScale;
+    if(!autoScale) updateSize(); // revert original size. 
   }
 
   public File getImageOn() {
@@ -79,7 +69,8 @@ public class ImageState extends JComponent implements OnOffInterface {
     // find off image
     if (this.imageOff == null) {
       File offFile = UIHelper.getOffFile(imageOn);
-      setImageOff(offFile);
+      if (offFile.exists())
+        setImageOff(offFile);
     }
 
     this.updateSize();
@@ -97,7 +88,9 @@ public class ImageState extends JComponent implements OnOffInterface {
 
   public void setImageOff(File imageOff) {
     this.imageOff = imageOff;
-    this.imageBufferOFF = UIHelper.readImg(imageOff, this);
+    if (imageOff != null && imageOff.exists()) { // OFF is optional...
+      this.imageBufferOFF = UIHelper.readImg(imageOff, this);
+    }
   }
 
   public void setActive(boolean value) {
@@ -109,18 +102,6 @@ public class ImageState extends JComponent implements OnOffInterface {
     return active;
   }
 
-  public String getCustomCmd() {
-    return customCmd;
-  }
-
-  public void setCustomCmd(String customCmd) {
-    this.customCmd = customCmd;
-  }
-
-  @Override
-  public boolean hasCustomCmd() {
-    return customCmd != null;
-  }
 
   @Override
   protected void paintComponent(Graphics g) {
@@ -146,10 +127,24 @@ public class ImageState extends JComponent implements OnOffInterface {
 
   }
 
+  private void drawImage(Graphics g, BufferedImage image) {
+
+    if (autoScale) {
+      g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+    } else {
+      g.drawImage(image, 0, 0, null);
+    }
+
+  }
+
   @Override
   public boolean contains(int x, int y) {
 
     boolean contains = super.contains(x, y);
+    
+    // if has focus in current component, ignore transparent shape
+    // this is used to allow resize.
+    if(this.hasFocus()) return contains;
 
     // Make component click precise, avoid click in transparent area..
     if (contains && imageBufferON != null) {
@@ -162,12 +157,10 @@ public class ImageState extends JComponent implements OnOffInterface {
     return contains;
 
   }
-
-  private void drawImage(Graphics g, BufferedImage image) {
-
-    g.drawImage(image, 0, 0, null);
-
-  }
+  
+  //===================================
+  // OnOffInterface
+  // ===================================
 
   @Override
   public void on() {
@@ -193,5 +186,5 @@ public class ImageState extends JComponent implements OnOffInterface {
   public boolean isOn() {
     return active;
   }
-
+  
 }
