f17993e61d47616e95605c3f025a59c1a72e3d83
Ricardo JL Rufino <ricardo.jl.rufino@gmail.com>
2020-07-01 20:35:59 -0300

    new widget - button

---
+++ RITController/src/main/java/com/ricardojlrufino/ritdevx/controller/widgets/skinnable/ImageButton.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * This file is part of RITDevX Controller project. Copyright (c) 2020 Ricardo JL Rufino.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, version 3 with Classpath
+ * Exception.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. See
+ * the included LICENSE file for details.
+ *******************************************************************************/
+package com.ricardojlrufino.ritdevx.controller.widgets.skinnable;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import javax.swing.JComponent;
+import com.ricardojlrufino.ritdevx.controller.utils.UIHelper;
+import com.ricardojlrufino.ritdevx.controller.widgets.ButtonInterface;
+import com.ricardojlrufino.ritdevx.controller.widgets.OnOffInterface;
+import com.ricardojlrufino.ritdevx.controller.widgets.OperationMode;
+
+/**
+ * This component uses two images to represent the ON and OFF states (optional). <br/>
+ * It can be used as a normal image, it can be used as a <b>button</b> (responding to clicks) and it
+ * can act as a <b>sensor</b>. It also has an optional 'CustomCmd' attribute, which identifies which
+ * command should be sent when it is pressed <br/>
+ * 
+ * @author Ricardo JL Rufino - (ricardo.jl.rufino@gmail.com)
+ * @date 11 de jun de 2020
+ */
+public class ImageButton extends ImageState implements ButtonInterface {
+
+  private static final long serialVersionUID = 1L;
+
+  private OperationMode operationMode = OperationMode.SWITCH;
+
+  private String customCmd;
+
+
+
+  //===================================
+  // OnOffInterface
+  // ===================================
+
+  @Override
+  public String getCustomCmd() {
+    return customCmd;
+  }
+
+  @Override
+  public void setCustomCmd(String customCmd) {
+    this.customCmd = customCmd;
+  }
+
+  @Override
+  public boolean hasCustomCmd() {
+    return customCmd != null;
+  }
+
+  public OperationMode getOperationMode() {
+    return operationMode;
+  }
+
+  public void setOperationMode(OperationMode mode) {
+    this.operationMode = mode;
+  }
+
+}
